d96544eb364c5221364291c3c44813a79ee45892
Lucas de Araujo <lucas@contreiras.info>
2026-05-11 08:49:00 -0300

    Create Vendavel interface for sellable items

---
+++ Vendavel.java
@@ -0,0 +1,8 @@
+public interface Vendavel {
+ 
+    // Retorna o preço do item vendável
+    public double getPreco();
+ 
+    // Retorna a descrição/nome do item vendável
+    public String getDescricao();
+}
