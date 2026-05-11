79be2d5b297aa1c255ce71420bc4322594acac36
Lucas de Araujo <lucas@contreiras.info>
2026-05-11 08:44:31 -0300

    Create Carro class extending Produto

---
+++ Carro.java
@@ -0,0 +1,6 @@
+public class Carro extends Produto {
+ 
+    public Carro(String descricao, String marca, double preco) {
+        super(descricao, marca, preco); // repassa tudo para o construtor de Produto
+    }
+}
